7dbe43d5a88704d4e2bcf282fb04c74e43e1c9f5
bgmi9900 <129384615+bgmi9900@users.noreply.github.com>
2024-08-25 21:25:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "918318996724"
+global.ownername = "🐛 MR RITESH "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
